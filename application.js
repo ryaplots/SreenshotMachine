42171ce3e24e4b8cbb206a9895c43f752e9025b1
ryaplots <56658938+ryaplots@users.noreply.github.com>
2020-06-01 03:38:47 +0100

    first commit

--- application.js
+++ application.js
@@ -1,45 +1,134 @@
 var screenshotmachine = require('screenshotmachine');
 var fs = require('fs');
-var customerKey = '1f6873';
-var secretPhrase = 'testphrase';
-var options = {
-    //mandatory parameter
-    url: '',
-    dimension: '1920x1080',
-    device: 'desktop',
-    format: 'jpg',
-    cacheLimit: '0',
-    delay: '500',
-    zoom: '100'
+const readline = require('readline');
+const { google } = require('googleapis');
+
+// Google Autentication
+const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+const TOKEN_PATH = 'token.json';
+
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+
+function authorize(credentials, callback) {
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
 }
 
-var companies = {
-    'iFunded': {
-        id: '1',
-        website: 'https://ifunded.de/en/'
-    },
-    'Property Partner': {
-        id: '2',
-        website: 'https://www.propertypartner.co'
-    },
-    'Property Moose': {
-        id: '3',
-        website: 'https://propertymoose.co.uk'
-    },
-    'Home Grown': {
-        id: '4',
-        website: 'https://www.homegrown.co.uk'
-    },
-    'Reality Mogul': {
-        id: '5',
-        website: 'https://www.realtymogul.com'
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
+}
+
+function uploadFile(auth, filePath) {
+    const drive = google.drive('v3');
+    const filesMetadata = {
+        'name': filePath
     }
+    const media = {
+        mimeType: 'image/jpg',
+        body: fs.createReadStream(filePath)
+    }
+    drive.files.create({
+        auth: auth,
+        resource: filesMetadata,
+        media: media
+    }, err => {
+        if (err) console.log(err);
+        else console.log('Uploaded!!');
+    })
 }
-for (var c in companies) {
-    options['url'] = companies[c]['website'];
-    companies[c]['apiURL'] = (screenshotmachine.generateScreenshotApiUrl(customerKey, secretPhrase, options));
-    var output = companies[c]['id'] + '_' + c + '.jpg';
-    screenshotmachine.readScreenshot(companies[c]['apiURL']).pipe(fs.createWriteStream(output).on('close', function() {
-        console.log('Screenshot saved as ' + output);
-    }));
-}
+
+
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    authorize(JSON.parse(content), function(auth) {
+
+
+        //
+        //Screenshot and upload
+        var customerKey = '1f6873';
+        var secretPhrase = 'testphrase';
+        var options = {
+            //mandatory parameter
+            url: '',
+            dimension: '1920x1080',
+            device: 'desktop',
+            format: 'jpg',
+            cacheLimit: '0',
+            delay: '500',
+            zoom: '100'
+        }
+
+        var companies = {
+            'iFunded': {
+                id: '1',
+                website: 'https://ifunded.de/en/'
+            },
+            'Property Partner': {
+                id: '2',
+                website: 'https://www.propertypartner.co'
+            },
+            'Property Moose': {
+                id: '3',
+                website: 'https://propertymoose.co.uk'
+            },
+            'Home Grown': {
+                id: '4',
+                website: 'https://www.homegrown.co.uk'
+            },
+            'Reality Mogul': {
+                id: '5',
+                website: 'https://www.realtymogul.com'
+            }
+        }
+        for (let c in companies) {
+            options['url'] = companies[c]['website'];
+            companies[c]['apiURL'] = (screenshotmachine.generateScreenshotApiUrl(customerKey, secretPhrase, options));
+            companies[c]['fileName'] = companies[c]['id'] + '_' + c + '.jpg';
+            screenshotmachine.readScreenshot(companies[c]['apiURL']).pipe(fs.createWriteStream(companies[c]['fileName']).on('close', function() {
+                console.log('Screenshot saved as ' + companies[c]['fileName']);
+                uploadFile(auth, companies[c]['fileName']);
+            }));
+        }
+    });
+});
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Drive Activity
     // API.
-    authorize(JSON.parse(content), listDriveActivity);
+    authorize(JSON.parse(content), uploadFiles);
 });
 
 /**
@@ -67,136 +67,6 @@ function getNewToken(oAuth2Client, callback) {
     });
 }
 
-/**
- * Lists the recent activity in your Google Drive.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listDriveActivity(auth) {
-    const service = google.driveactivity({ version: 'v2', auth });
-    const params = {
-        'pageSize': 10,
-    };
-    service.activity.query({ requestBody: params }, (err, res) => {
-        if (err) return console.error('The API returned an error: ' + err);
-        const activities = res.data.activities;
-        if (activities) {
-            console.log('Recent activity:');
-            activities.forEach((activity) => {
-                const time = getTimeInfo(activity);
-                const action = getActionInfo(activity.primaryActionDetail);
-                const actors = activity.actors.map(getActorInfo);
-                const targets = activity.targets.map(getTargetInfo);
-                console.log(`${time}: ${truncated(actors)}, ${action}, ` +
-                    `${truncated(targets)}`);
-            });
-        } else {
-            console.log('No activity.');
-        }
-    });
-}
+function uploadFile(auth) {
 
-/**
- * Returns a string representation of the first elements in a list.
- *
- * @param {Array<Object>} array The array to convert to a short string.
- * @param {number} limit The number of elements to show before truncating.
- * @return {string}
- */
-function truncated(array, limit = 2) {
-    const contents = array.slice(0, limit).join(', ');
-    const more = array.length > limit ? ', ...' : '';
-    return `[${contents}${more}]`;
-}
-
-/**
- * Returns the name of a set property in an object, or else "unknown".
- *
- * @param {Object} object The object in which to find the set property.
- * @return {string}
- */
-function getOneOf(object) {
-    for (const key in object) {
-        if (object.hasOwnProperty(key)) {
-            return key;
-        }
-    }
-    return 'unknown';
-}
-
-/**
- * Returns a time associated with an activity.
- *
- * @param {Object} activity The DriveActivity from which to extract a time.
- * @return {string}
- */
-function getTimeInfo(activity) {
-    if ('timestamp' in activity) {
-        return activity.timestamp;
-    }
-    if ('timeRange' in activity) {
-        return activity.timeRange.endTime;
-    }
-    return 'unknown';
-}
-
-/**
- * Returns the type of action.
- *
- * @param {Object} actionDetail The ActionDetail to summarize.
- * @return {string}
- */
-function getActionInfo(actionDetail) {
-    return getOneOf(actionDetail);
-}
-
-/**
- * Returns user information, or the type of user if not a known user.
- *
- * @param {Object} user The User to summarize.
- * @return {string}
- */
-function getUserInfo(user) {
-    if ('knownUser' in user) {
-        const knownUser = user.knownUser;
-        const isMe = knownUser.isCurrentUser || false;
-        return isMe ? 'people/me' : knownUser.personName;
-    }
-    return getOneOf(user);
-}
-
-/**
- * Returns actor information, or the type of actor if not a user.
- *
- * @param {Object} actor The Actor to summarize.
- * @return {string}
- */
-function getActorInfo(actor) {
-    if ('user' in actor) {
-        return getUserInfo(actor.user);
-    }
-    return getOneOf(actor);
-}
-
-/**
- * Returns the type of a target and an associated title.
- *
- * @param {Object} target The Target to summarize.
- * @return {string}
- */
-function getTargetInfo(target) {
-    if ('driveItem' in target) {
-        const title = target.driveItem.title || 'unknown';
-        return `driveItem:"${title}"`;
-    }
-    if ('drive' in target) {
-        const title = target.drive.title || 'unknown';
-        return `drive:"${title}"`;
-    }
-    if ('fileComment' in target) {
-        const parent = target.fileComment.parent || {};
-        const title = parent.title || 'unknown';
-        return `fileComment:"${title}"`;
-    }
-    return `${getOneOf(target)}:unknown`;
 }
